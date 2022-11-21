219ad847625375362365b85293711dd294a67a65
sharad9201 <sharad.rae19@gmail.com>
2022-11-22 00:18:13 +0545

    admin divied with roles in users table

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,8 @@
-home
+{{-- home --}}
+@extends('layouts.app')
+@section('content')
+    <a href="{{ route('books.index') }}" class="btn btn-primary">View Books</a>
+    @if (auth()->user()->isAdmin())
+        <a href="{{ route('books.create') }}" class="btn btn-primary">Add Books</a>
+    @endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BookController;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,10 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
 
-Route::get('/home',[HomeController::class,'index'])->name("home.index");
+Route::middleware(['auth'])->group(function () {
+    Route::resource('/books', BookController::class);
+});
+Route::get('/home', [HomeController::class, 'index'])->name("home.index");
+
 
 // Route::get('/book',[BookController::class,'index'])->name("book.index");
-Route::resource('/books',BookController::class);
-
-
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
